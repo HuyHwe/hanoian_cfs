bd843838042252eba58e4c1d30ce5698e764e680
Nguyễn Đức Huy <nguyenduchuy564@gmail.com>
2023-01-01 12:53:49 +0700

    add enter and done textarea visibility

--- assets/index.js
+++ assets/index.js
@@ -2,17 +2,18 @@ confess = document.getElementById("confess");
 listen = document.getElementById("listen");
 description = document.getElementById("description");
 title = document.getElementById("title");
-write = document.getElementById("write")
+write = document.getElementById("write");
+enter = document.getElementById("enter");
 
 
-console.log(confess);
 confess.addEventListener("click", toConfess);
 listen.addEventListener("click", toListen);
 
 function toConfess() {
+    write.style.display = "block";
     confess.style.display = "none";
     description.style.display = "none";
-    write.style.display = "flexbox";
+    enter.style.display = "block"
     title.style.top -= 30;
 }
 
