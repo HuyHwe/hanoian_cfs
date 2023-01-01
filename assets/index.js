e21562180549a70df7052925ff5e63042221cbf9
Nguyễn Đức Huy <nguyenduchuy564@gmail.com>
2023-01-01 12:27:00 +0700

    add cfs box

--- assets/index.js
+++ assets/index.js
@@ -2,13 +2,17 @@ confess = document.getElementById("confess");
 listen = document.getElementById("listen");
 description = document.getElementById("description");
 title = document.getElementById("title");
+write = document.getElementById("write")
+
+
 console.log(confess);
 confess.addEventListener("click", toConfess);
 listen.addEventListener("click", toListen);
 
 function toConfess() {
-    listen.style.display = "none"
-    description.style.display = "none"
+    confess.style.display = "none";
+    description.style.display = "none";
+    write.style.display = "flexbox";
     title.style.top -= 30;
 }
 
