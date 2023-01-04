38fcc3e3aa1617d3832307ec93e094dd736837ab
Nguyễn Đức Huy <112917549+HuyHwe@users.noreply.github.com>
2023-01-04 16:50:33 +0700

    delete dirname

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const path = require('path');
-__dirname = 'app/'
+
 
 const PORT = process.env.PORT || 5500;
 
