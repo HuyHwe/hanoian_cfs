7794005224acd07dd7416957052229093ce1c23f
Nguyễn Đức Huy <112917549+HuyHwe@users.noreply.github.com>
2023-01-04 16:34:39 +0700

    change __dirname

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const path = require('path');
-__dirname = 'D:/MyWebScript/Cfs/'
+__dirname = '../'
 
 const PORT = process.env.PORT || 5500;
 
