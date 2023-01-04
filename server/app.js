543de7c8609c9e31688ce4d6e82941a3074000b6
Nguyễn Đức Huy <nguyenduchuy564@gmail.com>
2023-01-04 20:57:45 +0700

    add error page and a database client

--- server/app.js
+++ server/app.js
@@ -2,9 +2,24 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const path = require('path');
-__dirname += "/../"
+const {Client} = require('pg');
+const e = require('express');
 
+__dirname += "/../"
 const PORT = process.env.PORT || 5500;
+const client = new Client({
+    host: "ec2-44-194-4-127.compute-1.amazonaws.com",
+    user: "ggfuetdphnkznr",
+    port: 5432,
+    password: "26a4000dd4191d1641d11d963309f1717fc5e16d6e5bd78ce32a37a66aaeed2b",
+    database: "dbvf991ujkjv9e",
+    ssl: {
+        require: true, // This will help you. But you will see new error
+        rejectUnauthorized: false // This line will fix new error
+      }
+});
+
+client.connect();
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use("/assets", express.static(__dirname + '/assets'));
@@ -13,8 +28,15 @@ app.get('/assets/index/', (req, res, next) => {
 
 })
 app.post('/assets/index/', (req, res, next) => {
-    console.log(req.body.cfs);
-    res.sendFile(path.join(__dirname+'assets/thanks.html'));
+    const cfs = req.body.cfs
+    client.query(`INSERT INTO quotes(cfs) VALUES ('${cfs}')`, (error, resolve) => {
+        if (error) {
+            console.log(error);
+            res.sendFile(path.join(__dirname + 'assets/error.html'));
+        } else {
+            res.sendFile(path.join(__dirname+'assets/thanks.html'));      
+        }
+    })
 })
 
 
